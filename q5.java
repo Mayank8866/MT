33ea4290ed1b25f25293329a02d128fab8a04a5e
mayank sahu <132261449+Mayank8866@users.noreply.github.com>
2023-07-02 13:53:05 +0530

    Create q5.java

---
+++ q5.java
@@ -0,0 +1,20 @@
+//question 5
+import java.util.*;
+class q5{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number/int ");
+         int n=sc.nextInt();
+
+        System.out.println("Enter number/double  ");
+        double d=sc.nextDouble();
+
+        System.out.println("Enter String ");
+        String str=sc.next();
+
+        System.out.println(n);
+        System.out.println(d);
+        System.out.println(str);
+
+    }
+}
